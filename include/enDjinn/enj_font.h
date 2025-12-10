f75587b904fe1bc22061ecbf207bf90028681e0c
Daniel Fairchild <daniel@fairchild.dk>
2025-12-10 09:04:15 +0100

    draft not working, going to the office

--- include/enDjinn/enj_font.h
+++ include/enDjinn/enj_font.h
@@ -5,6 +5,13 @@
 #include <enDjinn/enj_font_types.h>
 #include <enDjinn/enj_types.h>
 
+
+/**
+ * Set the Z value used for font rendering
+ * @param zvalue The Z value to use for rendering
+ */
+void enj_font_set_zvalue(float zvalue);
+
 /**
  * Load an enDjinn font from a memory blob
  * @param blob Pointer to font data blob
@@ -18,7 +25,7 @@ int enj_font_from_blob(const uint8_t* blob, enj_font_header_t* out_font);
  * @param out_font Pointer to store loaded font header
  * @return 1 on success, 0 on failure
  */
-int enj_font_load(const char* path, enj_font_header_t* out_font);
+int enj_font_from_file(const char* path, enj_font_header_t* out_font);
 
 /** Configure a PVR sprite header for rendering text glyphs
  * @param font Pointer to font header
@@ -88,7 +95,6 @@ int enj_font_glyph_uv_coords(enj_font_header_t* font, char glyph, uint32_t* auv,
  * @param font Pointer to font header
  * @param x X position to draw at in pixels
  * @param y Y position to draw at in pixels
- * @param zvalue Z value to use for the glyph
  * @param state_ptr Optional pointer to PVR draw state
  *
  * @return width of rendered glyph in pixels
@@ -96,7 +102,7 @@ int enj_font_glyph_uv_coords(enj_font_header_t* font, char glyph, uint32_t* auv,
  * @note Method assumes that the PVR sprite header has already been committed
  */
 int enj_font_render_glyph(char glyph, enj_font_header_t* font, uint16_t x,
-                          uint16_t y, float zvalue, pvr_dr_state_t* state_ptr);
+                          uint16_t y, pvr_dr_state_t* state_ptr);
 
 /**
  * Render a text string to the PVR command buffer
@@ -109,7 +115,7 @@ int enj_font_render_glyph(char glyph, enj_font_header_t* font, uint16_t x,
  * @return width of rendered text in pixels
  */
 int enj_font_render_text(const char* text, enj_font_header_t* font, uint16_t x,
-                         uint16_t y, float zvalue, pvr_dr_state_t* state_ptr);
+                         uint16_t y, pvr_dr_state_t* state_ptr);
 
 /** Render text within a bounding box
  * @param text Null-terminated string to draw
@@ -123,7 +129,7 @@ int enj_font_render_text(const char* text, enj_font_header_t* font, uint16_t x,
  * @return number of lines rendered
  */
 int enj_font_render_text_in_box(const char* text, enj_font_header_t* font,
-                                uint16_t min_x, uint16_t min_y, float zvalue,
+                                uint16_t min_x, uint16_t min_y,
                                 uint16_t box_width, uint16_t box_height,
                                 pvr_dr_state_t* state_ptr);
 
--- src/enj_debug.c
+++ src/enj_debug.c
@@ -1,6 +1,7 @@
 #include <enDjinn/enj_debug.h>
+#include <enDjinn/enj_draw.h>
 #include <enDjinn/enj_font.h>
-#include <enDjinn/render.h>
+#include <enDjinn/enj_render.h>
 
 // #define ENJ_ADD_DBG_FONT
 #ifdef ENJ_ADD_DBG_FONT
@@ -14,23 +15,21 @@ static pvr_sprite_hdr_t enj_debug_font_sprite_hdr;
 // #else
 // #endif
 #include <enDjinn/debug_font.h>
-#include "enj_debug.h"
 
 int enj_debug_init() {
-  enj_font_from_blob(enj_debug_font_data, &debug_font);
-  enj_font_PAL_OP_header(&debug_font, &enj_debug_font_sprite_hdr, 0,
-                      (enj_color_t){.raw = 0xffffffff},
-                      (enj_color_t){.raw = 0x00000000}, PVR_PAL_ARGB8888);
+  enj_font_from_blob(enj_debug_font_data, &enj_debug_font);
+  enj_font_PAL_OP_header(&enj_debug_font, &enj_debug_font_sprite_hdr, 0,
+                         (enj_color_t){.raw = 0xffffffff},
+                         (enj_color_t){.raw = 0x00000000}, PVR_PAL_ARGB8888);
   return 0;
 }
 
-int enj_debug_write_on_screen(const char *str, int x, int y, float zvalue)
-{
-    enj_renderlist_add(PVR_LIST_PT_POLY, NULL, NULL);
-    return enj_font_render_text(str, &debug_font, x, y, 10.0f, NULL);
+int enj_debug_write_on_screen(const char *str, int x, int y) {
+  enj_renderlist_add(PVR_LIST_PT_POLY, NULL, NULL);
+  return enj_font_render_text(str, &enj_debug_font, x, y, NULL);
 }
-else int enj_debug_init()
-{
-    return 0;
+void enj_debug_shutdown() {
+  // Nothing to do
 }
+
 #endif // ENJ_ADD_DBG_FONT
