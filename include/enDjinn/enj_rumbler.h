8da1a8be7e26107a6da58777dd7e8cfb5153808d
Daniel Fairchild <daniel@viamap.net>
2025-12-15 13:09:35 +0100

    Rumble Accessory Tester ported and working Unfortunately still getting dispapearing devices spuriously :-/

--- include/enDjinn/enj_rumbler.h
+++ include/enDjinn/enj_rumbler.h
@@ -2,6 +2,7 @@
 #define ENJ_RUMBLE_H
 
 #include <enDjinn/enj_ctrl.h>
+#include <dc/maple/purupuru.h>
 
 typedef enum uint8_t {
   enj_rumble_set = 1,
@@ -13,8 +14,14 @@ typedef enum uint8_t {
 
 void enj_rumble_set_rate_limit(int frames);
 
-enj_rumbler_reply_e enj_rumbler_set(enj_ctrl_port_name_e ctrloffset, uint32_t raw);
+enj_rumbler_reply_e enj_rumbler_set_effect(enj_ctrl_port_name_e ctrloffset, uint32_t raw);
 
-
+/**
+ * Get the current rumble device states
+ * @return Array of maple_device_t pointers for each rumble device
+ * @note The array has MAPLE_PORT_COUNT elements, corresponding to each port.
+ * @note A NULL pointer indicates no rumble device is connected on that port.
+ */
+maple_device_t** enj_rumbler_get_states();
 
 #endif // ENJ_RUMBLE_H
