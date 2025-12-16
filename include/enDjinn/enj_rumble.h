8fa9a25a7acc28f18898e325490059a6897833ea
Daniel Fairchild <daniel@viamap.net>
2025-12-16 22:14:27 +0100

    renamed enj_rumbler_* to enj_rumble_*

--- include/enDjinn/enj_rumble.h
+++ include/enDjinn/enj_rumble.h
@@ -10,11 +10,11 @@ typedef enum uint8_t {
   enj_rumble_rate_limited = 4,
   enj_rumble_overwrote_previous = 8,
   enj_rumble_unspecified_error = 16,
-} enj_rumbler_reply_e;
+} enj_rumble_reply_e;
 
 void enj_rumble_set_rate_limit(int frames);
 
-enj_rumbler_reply_e enj_rumbler_set_effect(enj_ctrl_port_name_e ctrloffset, uint32_t raw);
+enj_rumble_reply_e enj_rumble_set_effect(enj_ctrl_port_name_e ctrloffset, uint32_t raw);
 
 /**
  * Get the current rumble device states
@@ -22,6 +22,6 @@ enj_rumbler_reply_e enj_rumbler_set_effect(enj_ctrl_port_name_e ctrloffset, uint
  * @note The array has MAPLE_PORT_COUNT elements, corresponding to each port.
  * @note A NULL pointer indicates no rumble device is connected on that port.
  */
-maple_device_t** enj_rumbler_get_states();
+maple_device_t** enj_rumble_get_states();
 
 #endif // ENJ_RUMBLE_H
