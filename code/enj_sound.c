2b57c01d756b08edd87f848c104bafb74e805381
Daniel Fairchild <daniel@viamap.net>
2025-12-18 09:07:44 +0100

    sound integration complete

--- code/enj_sound.c
+++ code/enj_sound.c
@@ -1,48 +1,46 @@
 #include <enDjinn/enj_defs.h>
 #include <enDjinn/enj_sound.h>
+#include <stdio.h>
 
 #define DCAUDIO_IMPLEMENTATION
 #include <enDjinn/ext/dca_file.h>
 
-sfxhnd_t enj_sound_load_wav_file(const char* filename) {
-  sfxhnd_t handle = snd_sfx_load(filename);
-  if (handle == SFXHND_INVALID) {
-    ENJ_DEBUG_PRINT("Failed to load WAV sound: %s\n", filename);
-  }
-  return handle;
-}
-
-sfxhnd_t enj_sound_load_wav_blob(uint8_t* data) {
-  sfxhnd_t handle = snd_sfx_load_buf((char*)data);
-  if (handle == SFXHND_INVALID) {
-    ENJ_DEBUG_PRINT("Failed to load WAV sound from blob\n");
-  }
-  return handle;
-}
-
 sfxhnd_t enj_sound_load_dca_file(const char* filename) {
-  // sfxhnd_t handle = snd_sfx_load(filename);
-  // if (handle == SFXHND_INVALID) {
-  //   ENJ_DEBUG_PRINT("Failed to load DCA sound: %s\n", filename);
-  // }
+  FILE* f = fopen(filename, "rb");
+  if (f) {
+    fseek(f, 0, SEEK_END);
+    size_t filesize = ftell(f);
+    fseek(f, 0, SEEK_SET);
+    uint8_t data[filesize];
+    size_t amountread = fread(data, 1, filesize, f);
+    fclose(f);
+    if (amountread == filesize) {
+      return enj_sound_load_dca_blob(data);
+    } else {
+      ENJ_DEBUG_PRINT("enj_sound_load_dca_file: could not read entire file %s\n", filename);
+    }
+  } else {
+    ENJ_DEBUG_PRINT("enj_sound_load_dca_file: could not open file %s\n", filename);
+  }
   return SFXHND_INVALID;
 }
 
-sfxhnd_t enj_sound_load_dca_blob(uint8_t* data) {
+sfxhnd_t enj_sound_load_dca_blob(uint8_t* dca_data) {
+  fDcAudioHeader* data = (fDcAudioHeader*)dca_data;
 
-  fDcAudioHeader *dca = (fDcAudioHeader*)data;
-  printf("DCA fourcc: %.4s\n", dca->fourcc);
+  if (fDaValidateHeader(data)) {
+    uint8_t bitsize = (uint8_t[]){16, 8, 4}[fDaGetSampleFormat(data)];
 
-  if (fDaValidateHeader(dca)) {
-    sfxhnd_t handle = snd_sfx_load_buf(data + sizeof(fDcAudioHeader));
-    if (handle == SFXHND_INVALID) {
-      ENJ_DEBUG_PRINT("Failed to load DCA sound from blob\n");
+    int channels = fDaGetChannelCount(data);
+    if (channels == 2) {
+      return snd_sfx_load_raw_buf(fDaGetChannelSamples(data, 0), fDaCalcChannelSizeBytes(data),
+                                  fDaCalcSampleRateHz(data), bitsize, 2);
     }
-    return handle;
-  }
-  
 
-  return SFXHND_INVALID; 
+    return snd_sfx_load_raw_buf(fDaGetChannelSamples(data, 0), fDaCalcChannelSizeBytes(data),
+                                fDaCalcSampleRateHz(data), bitsize, 1);
+  }
+  return SFXHND_INVALID;
 }
 
 void enj_sound_unload(sfxhnd_t handle) { snd_sfx_unload(handle); }
